0a10805e15286a05581b64be928820f1cb9f12ad
sastrawangsa <2133242+angsagd@users.noreply.github.com>
2025-12-05 09:16:55 +0800

    Add cookie handling functionality with a demo interface

---
+++ session-cookie/index.php
@@ -0,0 +1,46 @@
+<?php
+$content = '';
+$uts = time();
+$status = 'Tuliskan isi cookie, kemudian tekan tombol simpan.';
+$latihan = '';
+
+if($_SERVER['REQUEST_METHOD']==='POST') {
+  if($content = $_POST['content']) {
+    setcookie('latihan', $content, time()+(60*60*24));
+    $status = 'Cookie sudah tersimpan.';
+  } else {
+    setcookie('latihan', '');
+    $status = 'Cookie sudah dihapus.';
+  }
+}
+
+if(isset($_COOKIE['latihan'])) {
+  $latihan = $_COOKIE['latihan'];
+}
+
+?><!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cookie Demo</title>
+</head>
+<body>
+  <header>
+    <h1>Cookie Demo</h1>
+  </header>
+  <main>
+    <form method="post">
+      <label for="input-content">Cookie [latihan] =</label>
+      <input type="text" id="input-content" name="content">
+      <button type="submit">Simpan Cookie</button>
+    </form>
+    <hr>
+    <p><?= $status ?> <a href="">Refresh</a></p>
+    <hr>
+    <p>Cookie Latihan = <?= $latihan ?></p>
+    <hr>
+    <p><?= $uts ?></p>
+  </main>
+</body>
+</html>
